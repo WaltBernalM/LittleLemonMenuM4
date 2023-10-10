ecfd6181cf477ba0494ae5624056cd9d411bff40
Walter Bernal <wltrbm@gmail.com>
2023-10-09 18:46:40 -0700

    Creation of LittleLemonLogo View

--- LittleLemonMenuM4/LittleLemonLogo.swift
+++ LittleLemonMenuM4/LittleLemonLogo.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct LittleLemonLogo: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image("littleLemon")
     }
 }
 
-#Preview {
-    LittleLemonLogo()
+struct LittleLemonLogo_Previews: PreviewProvider {
+    static var previews: some View {
+        LittleLemonLogo()
+    }
 }
