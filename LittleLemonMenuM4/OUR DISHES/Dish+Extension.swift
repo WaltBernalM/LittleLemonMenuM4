5f7e639e773f3211be3ddeff658c80f3f1e8f80a
Walter Bernal <wltrbm@gmail.com>
2023-10-10 04:15:34 -0700

    Dish.exists function fixed in Dish+Extension.swift

--- LittleLemonMenuM4/OUR DISHES/Dish+Extension.swift
+++ LittleLemonMenuM4/OUR DISHES/Dish+Extension.swift
@@ -11,5 +11,13 @@ import CoreData
 extension Dish {
     static func createDishesFrom(menuItems: [MenuItem], _ context: NSManagedObjectContext) {
         
+        do {
+            guard let results = try context.fetch(request) as? [Dish]
+            else { return nil }
+            return results.count > 0
+        } catch (let error){
+            print(error.localizedDescription)
+            return false
+        }
     }
 }
